23232a48d5c062bea34a55c339aa3ab606075e50
Carlos Sánchez <holapueblodev@gmail.com>
2023-01-11 09:18:06 +0100

    created function cart

--- script.js
+++ script.js
@@ -67,3 +67,62 @@
 
 // renderizarProductos();
 // renderizarCarrito();
+
+
+
+let DOMcart = document.querySelector('.main-cart');
+let DOMbuttonAdd = document.querySelector('#product-button');
+
+DOMbuttonAdd.addEventListener("click", AddtoCart())
+// console.info(products)
+
+
+
+
+
+// function PrintCart (){
+// }   
+   
+
+
+
+
+products.forEach((item) => {
+        let print = document.createElement('div');
+        print.innerHTML = `
+        <div class="itemCart" style="height: 15vh;">
+            <img src="${item.image}">
+            <div>
+                <h5>${item.name}</h5>
+                <button>borrar</button>
+            </div>
+            <div>
+                <button>-</button>
+                <p>cantidad</p>
+                <button>+</button>
+            </div>
+            <div>
+                <p>${item.price}€</p>
+            </div>
+            <div>
+                <p>total €</p>
+            </div>
+        </div>
+        `;
+    
+    DOMcart.append(print);
+})
+    
+
+
+let Cart = JSON.parse(localStorage.getItem("Cart")) || []
+
+function AddtoCart(newItem) {
+    switch (Cart.indexOf(newItem)){
+        case ('-1'):{
+            Cart.push(newItem)
+        }
+
+    }
+}
+
